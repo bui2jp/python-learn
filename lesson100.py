507b1507712064bd7e7dc1219aa5268ba027913e
takuya <>
2022-03-19 08:34:09 +0900

    add

---
+++ lesson100.py
@@ -0,0 +1,9 @@
+import glob
+import zipfile
+
+with zipfile.ZipFile('test.zip', 'w') as z:
+    #z.write('test_dir')
+    #z.write('test_dir/test.txt')
+    for f in glob.glob('test_dir/**', recursive=True):
+        print(f)
+        
